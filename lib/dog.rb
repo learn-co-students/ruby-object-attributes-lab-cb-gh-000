160c2fa1489bc7edd2c6eaf86fb2d884f0eeb234
Edson Mendieta <emendietaj@gmail.com>
2017-01-23 21:30:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+
+class Dog
+    def name=(dogName)
+        @name = dogName
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(dogBreed)
+        @breed = dogBreed
+    end
+
+    def breed
+        @breed
+    end
+end
