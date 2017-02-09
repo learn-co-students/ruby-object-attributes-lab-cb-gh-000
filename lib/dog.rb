623fad7382051be12eb02aec8f4d98c341482ad8
Bijan Fazeli <bfazeli1@gmail.com>
2017-02-09 22:30:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def initialize
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize
+    @name = name
+    @job = job
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(breed)
+    @job = breed
+  end
+
+  def job
+    @job
+  end
+end
