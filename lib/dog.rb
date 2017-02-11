0976b07fbbfcb5b6f00fa0463b4df25da13a4119
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2017-02-11 16:24:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+  def name #Alıcı yöntem
+
+    @name
+
+  end
+
+  def name=(name) #set yani atama yöntemi değer atıyor.
+
+    @name=name #Kullanıcıdan gelen name ' i  @name ' e set  ediyor
+
+  end
+
+
+  def breed
+
+    @breed
+
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name # Kullanıcıdan gelen name ' i atıyor . Setter yöntemi tabi.
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job 
+  end
+
+end
