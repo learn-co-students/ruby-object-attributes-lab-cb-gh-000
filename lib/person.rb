a00abd27a0f9191cc96b89a4db3bd982e9cec6c9
Shreyans Jain <gamingeyefinity@gmail.com>
2017-02-13 08:50:14 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+    def name=(name)
+        @name = name
+    end
+
+
+    def name
+        return @name
+    end
+
+
+    def job=(job)
+        @job = job
+    end
+
+
+    def job
+        return @job
+    end
+end
