48b47adf4b9d578e7099d67c8f973f0944e8bef3
Bilal Ünal <mysoul444@gmail.com>
2017-02-14 09:31:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_in)
+    @breed = breed_in
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(name_in)
+    @name = name_in
+  end
+  def name
+    @name
+  end
+  def job=(job_in)
+    @job = job_in
+  end
+  def job
+    @job
+  end
+end
