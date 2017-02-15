782ffed7e4c8a833caf684f223bf5dddb0ccdd70
Rafid Aslam <rafidteam@gmail.com>
2017-02-15 16:01:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def name=(dogname)
+        @name = dogname
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breedname)
+        @breed = breedname
+    end
+
+    def breed
+        @breed
+    end
+
+end
