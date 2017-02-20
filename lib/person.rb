4424759fce682bbe13018fc0dd9eeb6d62312842
Ivan Baranov <mr.jack.jones@yandex.ru>
2017-02-20 08:36:49 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name=(name_dog)
+    @name = name_dog
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_name)
+    @job =  job_name
+  end
+  def job
+    @job
+  end
+end
