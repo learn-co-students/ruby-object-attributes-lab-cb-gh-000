616769aedf2e92fa26ac38a96c1e4aa8f8fd6339
Demi Conductor <demiconductor@outlook.com>
2017-03-31 15:05:05 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+
+  def job=(job_name)
+    @job = job_name
+  end
+  def job
+    @job
+  end
+end
