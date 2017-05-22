f6e47720ba9622fcf448fdf303df5d8bd5904a3b
cristopher rivas <cristopherjrivasc@gmail.com>
2017-05-22 04:30:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def name=(s)
+    @name = s
+  end
+
+  def  name
+    return @name
+  end
+
+  def breed=(s)
+    @breed = s
+  end
+
+  def  breed
+    return @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+
+  def name=(s)
+    @name = s
+  end
+
+  def  name
+    return @name
+  end
+
+  def job=(s)
+    @job = s
+  end
+
+  def  job
+    return @job
+  end
+end
