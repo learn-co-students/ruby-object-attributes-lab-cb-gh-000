b1244e4aeb30b6e999ac3f26a1899f8473239290
Sam Ebertz <samuel.ebertz@gmail.com>
2017-05-23 18:10:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(str)
+    @name = str
+  end
+  def name
+    return @name
+  end
+  def breed=(str)
+    @breed = str
+  end
+  def breed
+    return @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(str)
+    @name = str
+  end
+  def name
+    return @name
+  end
+  def job=(str)
+    @job = str
+  end
+  def job
+    return @job
+  end
+end
