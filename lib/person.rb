a0a576f7a786a8f5254a6d78ddf58d837e9fb58f
Sri Harsha Gajavalli <sriharsha.g15@iiits.in>
2017-06-03 06:36:30 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(dog_breed)
+    @job = dog_breed
+  end
+
+  def job
+    @job
+  end
+end
+
+  
