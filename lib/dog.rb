d8ca32572b83390159bcd27620f0c8a4732afaff
Kajan Mohanagandhirasa <kajan.14@cse.mrt.ac.lk>
2017-06-03 11:31:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def instantiate(name, beed)
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
+  
+    
