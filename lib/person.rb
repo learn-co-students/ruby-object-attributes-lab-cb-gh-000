cb65e3170b2578e01922fc187c1e1415561b326f
Daan Schutte <daanskitte@gmail.com>
2017-06-12 10:57:22 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+  def job
+    @job
+  end
+
+end
