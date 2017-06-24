2da39f9556977bac72660fbb0bd8a25568b96274
Wes Thomspon <wes977@gmail.com>
2017-06-24 05:11:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  #def initialize(name, breeds)
+  #  @name = name
+  #  @breed = breeds
+  #end
+ # Setters
+  def name=(name)
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+ # Getters
+ def name
+   "#{@name}"
+ end
+ def breed
+   "#{@breed}"
+ end
+end
