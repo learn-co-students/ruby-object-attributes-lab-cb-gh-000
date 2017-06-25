9b840e7e6314edb93c7a84e841a3b9aeda9282d4
Didi Roesmana <didileeee@gmail.com>
2017-06-25 16:26:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(str)
+    @name=str
+  end
+  def name
+    @name
+  end
+  def breed=(str)
+    @breed=str
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(str)
+    @name=str
+  end
+  def name
+    @name
+  end
+  def job=(str)
+    @job=str
+  end
+  def job
+    @job
+  end
+end
