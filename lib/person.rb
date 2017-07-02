a0e43cd0ca06880d91b7516d5ae4f63b78ff420c
Shekh Ataul <ataul.one@gmail.com>
2017-07-02 18:40:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+  def job=(new_job_name)
+    @job = new_job_name
+  end
+  def job
+      @job
+  end
+
+end
