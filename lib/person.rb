575025781921c1a83bdd5cddd308ba8a73509dc4
Jamie Roberts <ll13j7r@leeds.ac.uk>
2017-07-14 13:58:43 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize()
+    @name = name
+    @job = job
+
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
