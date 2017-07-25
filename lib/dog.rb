a8eda35f2cb778f73229f54ea37fcabcb4ef09bc
Akram Taha <akram.eltranesy95@eng-st.cu.edu.eg>
2017-07-25 20:19:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  def name(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+
+  def name=newName
+    @name=newName
+  end
+
+
+  def bread(breed)
+    @breed=breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=newBreed
+    @breed=newBreed
+  end
+
+end
