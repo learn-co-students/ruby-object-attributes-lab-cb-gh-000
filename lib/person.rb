55c5b92d747fbe14548fb3f6ecbc75c0d567ac99
Armando Marquez <amarquezmazz@mail.usf.edu>
2017-07-26 08:51:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(insert_name)
+    @name = insert_name
+  end
+  def job=(insert_job)
+    @job = insert_job
+  end
+
+  def name
+    @name
+  end
+  def job
+    @job
+  end
+end
