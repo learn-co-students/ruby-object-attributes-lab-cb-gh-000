747d07a7af2fbfdbde4577ef584e76682d52e5e8
Muhammad Andriansyah <muh.andriansyah97@gmail.com>
2017-07-28 15:16:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(name_dog)
+    @name = name_dog
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(insert_breed)
+    @breed = insert_breed
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(your_name)
+    @name = your_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(your_job)
+    @job = your_job
+  end
+
+  def job
+    @job
+  end
+end
