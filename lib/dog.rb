16f6a64fa4b816e78423175b75c1e74e049a47d6
Daniel Constantine <danielkevinconstantine@gmail.com>
2017-08-10 23:37:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+
+  end
+end
+
+class Person
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+end
