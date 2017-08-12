c5328f01a491699dbde6fa78416b49fa515f14da
Melissa Sheehan <mxsheehan7@gmail.com>
2017-08-12 01:08:05 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_name)
+    @job = job_name
+  end
+
+  def job
+    @job
+  end
+end
