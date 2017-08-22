8967c215e732af6a07d1513c866ca9d346815a64
Cem Gunes <cemguness@live.com>
2017-08-22 22:03:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(new_name)
+    @name=new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breedy)
+    @breed=breedy
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name=(person_name)
+    @name=person_name
+  end
+  def name
+    @name
+  end
+
+  def job=(metier)
+    @job=metier
+  end
+
+  def job
+    @job
+  end
+end
