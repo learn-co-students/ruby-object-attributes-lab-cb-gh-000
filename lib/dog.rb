44cc5acbe6ee0cf275ce30dfe9d3d6929be075f5
Rana Azmy <rana.azmy95@hotmail.com>
2017-09-19 22:42:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def name=(name)
+  def name= (name)
     @name = name
   end
   def name
     @name
   end
-  def breed = (breed)
+  def breed= (breed)
     @breed = breed
   end
-
-
-
+  def breed
+    @breed
+  end
 end
