62ceb573d2832ac8ee9e4050942c752f2401d610
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-22 06:49:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+end
