d928a05d72aea6fabc227ae368f4b1c28454b257
Dominic Chapman <dominicchapman@me.com>
2017-09-27 22:25:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+
+class Dog
+  
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
