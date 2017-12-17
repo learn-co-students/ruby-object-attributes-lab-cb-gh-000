e4768373bce5f53c5d227e1d8e15e35325c69d27
Umair Ahmad <ahmadumair17@gmail.com>
2017-12-17 21:59:10 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(arg_name)
+    @name = arg_name
+  end
+  def name()
+    @name
+  end
+
+  def job=(arg_job)
+    @job = arg_job
+  end
+  def job()
+    @job
+  end
+end
