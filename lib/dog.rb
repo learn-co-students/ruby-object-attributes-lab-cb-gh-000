aefbb2f8558a976f0bf2a4dd9e0c3a2e6de9ac42
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-27 06:54:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(inputName)
+    @name = inputName
+  end
+  def name
+    @name
+  end
+  def breed=(inputBreed)
+    @breed = inputBreed
+  end
+  def breed
+    @breed
+  end
+end
