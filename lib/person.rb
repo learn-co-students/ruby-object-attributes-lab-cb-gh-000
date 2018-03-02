b39523610aafdc13cbe8e82dec8c23dafd099d7b
norman lewis <takialog@gmail.com>
2018-03-02 02:13:12 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
-  def name=(name)
-    @name = name
+  def name=(person_name)
+    @name = person_name
   end
 
   def name
     @name
   end
 
-  def job=(job)
-    @job = job
+  def job=(person_job)
+    @job = person_job
   end
 
   def job
