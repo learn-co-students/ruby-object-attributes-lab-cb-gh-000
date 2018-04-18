94f078ff9bece517508536feb60668a141349080
Mason Merril <puckmaster24@yahoo.com>
2018-04-18 21:28:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(user_name)
+    @name = user_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(user_job)
+    @job = user_job
+  end
+
+  def job
+    @job
+  end
+
+end
