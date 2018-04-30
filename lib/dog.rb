19155c5d74d376f8267dfd7929d7fd805f992646
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-04-30 15:48:44 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def intilize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def initial(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def init(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(name)
+    @name = name
+  end
+
+
+  def intj(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job=(job)
+    @job = job
+  end
+end
