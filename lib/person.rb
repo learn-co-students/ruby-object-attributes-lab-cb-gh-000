2066380b7f75f9ca53c0a25b79ada958fd6103d7
Pablo Peña <pablo@makeitworth.com.mx>
2018-05-13 03:20:50 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    @job
+  end
+
+end
