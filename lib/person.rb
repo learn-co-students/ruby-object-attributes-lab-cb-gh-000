a20f751339afc96fc1515d105f14dea27d453898
Kyle Berkley <berkyle@gmail.com>
2018-05-17 23:26:47 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+  # def initialize(name, job)
+  #   @name = name
+  #   @job = job
+  # end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+end
