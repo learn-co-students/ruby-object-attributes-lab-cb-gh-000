49e1bf02ba78f9b00855f4943ef3c747b6e512e3
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-16 18:50:12 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name_of_person)
+  @name  = name_of_person
+  end
+
+  def name
+    @name
+  end
+
+  def job=(name_of_job)
+    @job = name_of_job
+  end
+
+  def job
+    @job
+  end
+end
