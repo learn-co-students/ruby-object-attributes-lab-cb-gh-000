b9a8abee10d6ca0352e2a9e80ea514133250114c
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-28 23:31:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+end
