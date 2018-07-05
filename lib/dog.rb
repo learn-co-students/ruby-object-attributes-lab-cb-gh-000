c515366847e5a21caf122116ab6bc00c2a6cd877
Enmanuel Gómez <lizardgreed@gmail.com>
2018-07-05 17:14:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+
+  def name
+    puts @dog_name
+  end
+
+  def breed=(dog_breed)
+    @dog_breed = dog_breed
+  end
+
+  def breed
+    puts @dog_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @person_name = person_name
+  end
+
+  def name
+    puts @person_name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    puts @job
+  end
+end
