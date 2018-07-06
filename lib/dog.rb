67aef30f69cb3bde4a00cc4f4e518b8840081d34
Lucas Wang <lucas211w@gmail.com>
2018-07-06 04:27:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(dogname)
+    @name = dogname
+  end
+  def breed
+    @breed
+  end
+  def breed=(dogbreed)
+    @breed = dogbreed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name
+    @name
+  end
+  def name=(personname)
+    @name = personname
+  end
+  def job
+    @job
+  end
+  def job=(personjob)
+    @job = personjob
+  end
+end
