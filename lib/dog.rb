8cedc063f979ea291e1020979346c0ee2f91fd35
Roberto Garza <robertg4567@hotmail.com>
2018-07-12 19:17:00 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+ #GETTER
+  def name
+    @name
+  end
+  #SETTER
+  def name=(new_name)
+    @name = new_name
+  end
+  #GETTER
+   def breed
+     @breed
+   end
+   #SETTER
+   def breed=(new_breed)
+     @breed = new_breed
+   end
+end
