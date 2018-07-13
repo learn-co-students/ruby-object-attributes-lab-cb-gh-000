190531e14bebc7ec7198182c242658f82cd39e1d
M G <marcela.guerra@gmail.com>
2018-07-13 18:15:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+
+  def breed
+    @breed
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+end
