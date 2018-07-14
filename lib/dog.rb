dae79a2c9e440ea8ea0c8b1a00f9f95c30ccf2ed
Hendrik Tammekivi <tammekivi.hendrik@gmail.com>
2018-07-14 12:47:00 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+  def initalize(name)
+    @name = name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+
+  def initalize(breed)
+    @breed = breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  def initalize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def initalize(job)
+    @job = job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+    
+end
