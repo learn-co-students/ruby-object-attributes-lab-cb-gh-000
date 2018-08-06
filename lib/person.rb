e3eca96e407a7833df99a3f276014bfb8433b1a9
Amin Ullah <aullah@umich.edu>
2018-08-06 17:44:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(get_name)
+    @name = get_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(get_job)
+    @job = get_job
+  end
+
+  def job
+    @job
+  end
+
+end
