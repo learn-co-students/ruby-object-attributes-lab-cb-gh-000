4e921f08a657eca912ece875acef754483238511
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 14:34:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,18 @@
 class Dog
-  def initialize(name)
+
+  def name=(name)
     @name = name
   end
+
   def name
     @name
   end
-  def name=(new_name)
-    @name = new_name
-  end
 
-  def initialize(breed)
+  def breed=(breed)
     @breed = breed
   end
+
   def breed
     @breed
-  end
-  def breed=(new_breed)
-    @breed = new_breed
   end
 end
