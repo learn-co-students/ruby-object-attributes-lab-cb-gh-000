dc5b54fb4c0e9567e2f9a5d41de35cc100f917a1
md mia <sharifmia091@gmail.com>
2018-08-28 05:30:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+  def breed
+    @breed
+  end
+end
