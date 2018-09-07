0cd1946e550bef3fe233feb6c321c0b622b17b2a
fusion MO <pzj1218331400@gmail.com>
2018-09-07 06:04:17 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize
+
+  end
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+end
