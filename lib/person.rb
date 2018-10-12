08d951725e22e0c6e73638a8f169d32eb6bc0f38
John Casabar <johncasabar@gmail.com>
2018-10-12 20:15:42 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize
+    @name
+    @job
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+end
