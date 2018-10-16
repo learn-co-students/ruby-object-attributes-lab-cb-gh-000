ceae753e0347408860c7088630ec43c87b874dc3
Angelo zarate <mrlzarate8@gmail.com>
2018-10-16 09:23:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name
+    @name
+  end
+  def name=(this_dog)
+    @name=this_dog
+  end
+  def breed
+    @breed
+  end
+  def breed=(this_breed)
+    @breed=this_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name
+    @name
+  end
+  def name=(this_dude)
+    @name=this_dude
+  end
+  def job
+    @job
+  end
+  def job=(this_job)
+    @job=this_job
+  end
+end
