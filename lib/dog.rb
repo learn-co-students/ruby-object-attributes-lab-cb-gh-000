da9404e315263041a4c4efc143a361dad8a3f378
Joseph Corona <josed.hc96@gmail.com>
2018-11-05 02:00:42 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  @name
+  @breed
+
+  def name=(dog_name)
+
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(_breed)
+
+    @breed = _breed
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+  @name
+  @job
+
+  def name=(dog_name)
+
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(dog_name)
+
+    @job = dog_name
+  end
+
+  def job
+    @job
+  end
+end
