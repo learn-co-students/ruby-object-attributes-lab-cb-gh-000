2442ed17979d9ffbd1295b8480ec43467badb26c
Evi Alvanaki <evialvanaki@gmail.com>
2018-12-03 08:52:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+def  name
+  @name
+end
+def name=(dog_name)
+  @name= dog_name
+end
+def breed=(bree)
+  @breed=bree
+end
+def breed
+  @breed
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+def name=(namie)
+  @name=namie
+end
+def name
+  @name
+end
+def job=(jobie)
+  @job=jobie
+end
+def job
+  @job
+end
+
+
+end
