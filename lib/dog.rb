ff3fde87e4fadf231a63950acda4f788ec3e3107
khalid Lamsadi <khalid.lamsadi@outlook.fr>
2018-12-09 11:50:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def name=(name_dog)
+      @name = name_dog
+    end
+
+    def name
+      @name
+    end
+
+    def breed
+      @breed
+    end
+
+    def breed=(bread_dog)
+      @breed = bread_dog
+    end
+end
