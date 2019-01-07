27c6569405602ad951d2d9f5d44da703562c6245
Anmol Jande <jandanmol@gmail.com>
2019-01-07 03:07:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,31 @@
+class Person
+  
+  def name
+    @name 
+  end 
+  def name=(n)
+    @name=n 
+  end 
+  
+   def job=(j)
+     @job=j 
+   end 
+   def job
+     @job 
+   end 
+   
+end 
+  def name
+    @name 
+  end 
+  def name=(n)
+    @name=n 
+  end 
+  
+   def job=(j)
+     @job=j 
+   end 
+   def job
+     @job 
+   end 
+   
