26eeded1ad00b3991357474e15a4976caa76a3ae
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-23 04:08:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(the_name)
+    @name = the_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(the_breed)
+    @breed = the_breed
+  end
+end
