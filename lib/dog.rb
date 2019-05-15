68a7ed134d72b8b2f2503121a8b2cb67ca02e67c
Damion Mounts <damionotg@gmail.com>
2019-05-15 21:52:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
