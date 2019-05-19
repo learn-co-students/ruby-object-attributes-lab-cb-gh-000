0d988c055b31da6fdcef5e3a30f36c1f006835c8
Shijie Li <dp0x7ce@gmail.com>
2019-05-19 13:30:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name = (new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+
+  def breed = (one_person)
+    @breed = one_person
+  end
+  def breed
+    @breed
+  end
+end
