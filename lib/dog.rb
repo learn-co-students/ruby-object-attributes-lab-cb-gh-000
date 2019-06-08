e3238379eeea750859f87015308156d0b5a96315
Satyam Dahiwal <therealsatyam@gmail.com>
2019-06-08 10:24:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(given_name)
+    @name = given_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(given_breed)
+    @breed = given_breed
+  end
+end
