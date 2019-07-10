84fddac5d2b2b4d9f1defedb98759fa07f8231ab
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 14:16:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(nameNew)
+    @name = nameNew
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breedTypeNew)
+    @breed = breedTypeNew
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(nameNew)
+    @name = nameNew
+  end
+
+  def job
+    @job
+  end
+
+  def job=(jobTypeNew)
+    @job = jobTypeNew
+  end
+end
