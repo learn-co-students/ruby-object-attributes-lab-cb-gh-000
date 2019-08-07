b365570f65a6cc81b5501d9caaeab349035dd8a9
Abdullah Ahmed <abdullah@softcomnetworks.com>
2019-08-07 06:59:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+# dog = Dog.new
+# dog.name = "fido"
+# puts dog.name
+# dog.breed = "German"
+# puts dog.breed
