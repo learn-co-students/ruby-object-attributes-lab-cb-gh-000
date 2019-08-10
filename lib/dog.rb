af0e9e939922593cf522d490348ee1ca9bdd53c8
Kyle Zheng <kylezheng73@gmail.com>
2019-08-10 03:50:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+end
