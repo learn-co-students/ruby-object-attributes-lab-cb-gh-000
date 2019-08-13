95ee576e04b693df9df7519ef0af63c65e610cec
tolga inkaya <inkayat@itu.edu.tr>
2019-08-13 12:52:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(_name)
+    @name = _name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(br)
+    @breed = br
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(_name)
+    @name = _name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(_job)
+    @job = _job
+  end
+end
