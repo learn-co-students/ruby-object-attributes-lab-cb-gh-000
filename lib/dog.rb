add4ce976d41be90a14e5a1ce028723f2395f98e
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-08-25 17:10:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name
+    return @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    return @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
