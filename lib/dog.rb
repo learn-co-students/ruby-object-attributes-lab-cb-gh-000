12e76717806087de24a71d77a68b725ec18d08e5
Sebastian Waetzold <swaetzold@gmx.de>
2019-08-26 11:32:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,14 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
   def name
     @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def breed
+    @breed
+  end
+  def breed=(new_name)
+    @breed = new_name
   end
 end
