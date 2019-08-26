9506c90298cc50965462072ee2872ad6a49044c3
Sebastian Waetzold <swaetzold@gmx.de>
2019-08-26 11:19:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+end
