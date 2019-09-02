8551c85c81588ccccee160fb2f44e5d253e7fc15
Edgar V <evenegas9@ucmerced.edu>
2019-09-02 03:26:29 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  #setter method
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  #getter method
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+end
