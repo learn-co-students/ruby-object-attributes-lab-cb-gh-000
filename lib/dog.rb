9a9f2aa1c00e68383c4661e70192de49c4aa05da
Wyatt Goodwin <wog6@cornell.edu>
2019-09-09 15:50:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def initname(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name= (new_name)
+    @name = new_name
+  end
+  def initbreed(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def breed= (new_breed)
+    @breed = new_breed
+  end
+end
