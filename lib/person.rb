20cf5e1761b6366944843e7aa7c6761436915e94
Pavel Torbin <pabin@yandex.ru>
2019-11-01 11:26:25 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+
+  def name=(per_name)
+    @name = per_name
+  end
+
+  def name
+    "#{@name}"
+  end
+  def job=(per_job)
+    @job = per_job
+  end
+
+  def name
+    "#{@job}"
+  end
+end
