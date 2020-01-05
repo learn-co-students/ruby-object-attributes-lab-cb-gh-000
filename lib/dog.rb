c290588dfecec7e1e2a8531adb0a0e90657319b9
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-05 23:49:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+
+  end
+  def breed=(dog_name)
+    @breed = dog_name
+  end
+  def breed
+    @breed
+
+  end
+end
