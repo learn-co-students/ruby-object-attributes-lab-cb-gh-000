fce8384820cf73135affa5453e2d0a868228170a
Rina Iosad <rina.iosad@gmail.com>
2020-01-22 21:22:14 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  #def initialize(name)
+  #  @name = name
+#    @breed = breed
+#  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+end
